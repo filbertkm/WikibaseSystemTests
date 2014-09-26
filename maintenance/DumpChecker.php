5b09dcd99db2c96b868708518963c21b6c19c666
aude <aude.wiki@gmail.com>
2014-09-26 11:29:36 +0200

    Move dump checker script

--- maintenance/DumpChecker.php
+++ maintenance/DumpChecker.php
@@ -1,9 +1,14 @@
 <?php
 
-namespace Wikibase\Test;
+namespace WikibaseSystemTests\Maintenance;
 
 use Wikibase\DataModel\Entity\Item;
 use Wikibase\Repo\WikibaseRepo;
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+$basePath = $IP !== false ? $IP : __DIR__ . '/../../..';
+
+require_once "$basePath/maintenance/Maintenance.php";
 
 /**
  * @group Wikibase
@@ -11,19 +16,19 @@ use Wikibase\Repo\WikibaseRepo;
  *
  * @licence GNU GPL v2+
  */
-class DumpSerializationTest extends \PHPUnit_Framework_TestCase {
+class DumpChecker extends \Maintenance {
 
 	private static $dbConn;
 
 	private $codec;
 
-	public function testDeserializeBlob() {
+	public function execute() {
 		$pgConn = $this->getDbConn();
 
-		$min = 0;
-		$max = 1000;
+		$min = 1000000;
+		$max = 1001000;
 
-		while ( $max <= 100000 ) {
+		while ( $max <= 5000000 ) {
 			$sql = "SELECT id FROM items WHERE id >= $min AND id < $max ORDER BY id";
 
 			$min = $min + 1000;
@@ -36,7 +41,11 @@ class DumpSerializationTest extends \PHPUnit_Framework_TestCase {
 				$json = $this->fetchEntityBlob( $row['id'] );
 				$this->assertCanDeserialize( $json, $itemId );
 			}
+
+			$this->output( "Processed up to $itemId\n" );
 		}
+
+		$this->output( "Done: $itemId\n" );
 	}
 
 	private function assertCanDeserialize( $json, $itemId ) {
@@ -48,8 +57,6 @@ class DumpSerializationTest extends \PHPUnit_Framework_TestCase {
 			echo "$itemId failed\n";
 			throw $ex;
 		}
-
-		$this->assertTrue( true );
 	}
 
 	private function getDbConn() {
@@ -86,5 +93,7 @@ class DumpSerializationTest extends \PHPUnit_Framework_TestCase {
 		throw new \Exception( 'entity not found' );
 	}
 
-
 }
+
+$maintClass = 'WikibaseSystemTests\Maintenance\DumpChecker';
+require_once RUN_MAINTENANCE_IF_MAIN;
